d1eeefff45aa93972ad90935fb076ed2b4596cb1
isaLR222 <isabellodeiroramos@gmail.com>
2026-04-28 22:53:24 +0200

    login y registro

--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,15 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('index');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
+
+    Route::get('/index', function () {
+        return view('index');
+    })->name('index');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
