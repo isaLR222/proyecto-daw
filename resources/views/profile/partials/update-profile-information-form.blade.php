f448949598832aab8a109771f3c31e6c993934f5
isaLR222 <isabellodeiroramos@gmail.com>
2026-05-13 19:18:54 +0200

    Editar perfil usuario

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -1,11 +1,11 @@
 <section>
     <header>
-        <h2 class="text-lg font-medium text-gray-900">
-            {{ __('Profile Information') }}
+        <h2 class="text-lg font-medium text-white">
+            {{ __('Datos del perfil') }}
         </h2>
 
-        <p class="mt-1 text-sm text-gray-600">
-            {{ __("Update your account's profile information and email address.") }}
+        <p class="mt-1 text-sm text-white">
+            {{ __("Actualiza tus datos") }}
         </p>
     </header>
 
